67d8abf5af2fcf208e28c89c427e73668417834c
Yash Thakre <yashthakre@Yashs-MacBook-Air.local>
2022-10-14 17:03:58 +0530

    WHile working

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
+from asyncore import write
 import pytchat
-import time
-import json
+from timeit import default_timer as timer
+from random import randint
 
-chat = pytchat.create(video_id="vRFrMnCOwlQ")
-while chat.is_alive():
-    for c in chat.get().sync_items():
-        # print(c.amountValue)
-        print(c.message)
-    # time.sleep(1)
+import csv
+
+def update_session_stats(chunk):
+    global total_chats
+    global total_donation
+
+    total_chats += len(chunk)
+
+    for c in chunk:
+        total_donation += float(c.amountValue)
+
+
+header = ['time','rating']
+# datafile = open('./data.csv', 'w')
+
+with open('./data.csv', 'w') as datafile:
+    writer = csv.writer(datafile)
+    writer.writerow(header)
+    datafile.close()
+
+    chat = pytchat.create(video_id="vRFrMnCOwlQ")
+    # chat = pytchat.create(video_id="eOuqg7_5DZc")
+    chunks = []
+    chunk_start = timer() 
+
+    total_donation = 0
+    time_elapsed = timer()
+    total_chats = 0
+
+
+
+    while chat.is_alive():
+        for c in chat.get().sync_items():
+            # print(c.amountValue)
+            cur_time = timer()
+            if cur_time - chunk_start > 10:
+                # print(chunks)
+                update_session_stats(chunks)
+                writer.writerow([timer() - time_elapsed , randint(-1,1)])
+                chunk_start = timer()
+                chunks = []
+                chunks.append(c)
+            # print(c.message)
+            else:
+                chunks.append(c)
+            print(f"> Time: {round(timer() - time_elapsed, 4)}\t| Donation : {total_donation}\t| Chats : {total_chats}")
